f676da2d7ebba13aac3cdcc32a8d51e47205e2eb
Marcelina Sitko <marcelina.sitko@interia.pl>
2020-12-07 14:16:59 +0100

    finish classes in third exercise

--- cs-lab04-Implementacje-interfejsow-implicit-explicit-kompozycja/Zadanie3/MultifunctionalDevice.cs
+++ cs-lab04-Implementacje-interfejsow-implicit-explicit-kompozycja/Zadanie3/MultifunctionalDevice.cs
@@ -1,8 +1,29 @@
 ﻿using System;
 namespace Zadanie3
 {
-    public class MultifunctionalDevice
+    public class MultifunctionalDevice: Copier
     {
-        
+        public Fax Fax { get; private set; }
+        public int FaxCounter => Fax.SendFaxCounter;
+
+        public MultifunctionalDevice()
+        {
+            Fax = new Fax();
+        }
+
+        public void SendFax(in IDocument document, string faxNumber)
+        {
+            if (state == IDevice.State.on)
+            {
+                Fax.PowerOn();
+                Fax.SendFax(in document, faxNumber);
+                Fax.PowerOff();
+            }
+        }
+        public void ScanAndSendFax(string faxNumber)
+        {
+            Scan(out IDocument document);
+            SendFax(document, faxNumber);
+        }
     }
 }
--- cs-lab04-Implementacje-interfejsow-implicit-explicit-kompozycja/Zadanie3/Program.cs
+++ cs-lab04-Implementacje-interfejsow-implicit-explicit-kompozycja/Zadanie3/Program.cs
@@ -6,12 +6,29 @@ namespace Zadanie3
     {
         static void Main(string[] args)
         {
+            var device = new MultifunctionalDevice();
+            device.PowerOn();
+            IDocument doc = new PDFDocument("aaaa.pdf");
+            device.Print(in doc);
+
+            IDocument doc2;
+            device.Scan(out doc2);
+            device.ScanAndPrint();
+            
+            device.SendFax(doc, "123456789");
+            device.ScanAndSendFax("123456789");
+            
+            Console.WriteLine(device.Counter);
+            Console.WriteLine(device.FaxCounter);
+            Console.WriteLine(device.PrintCounter);
+            Console.WriteLine(device.ScanCounter);
+            
             var copier = new Copier();
             copier.PowerOn();
             IDocument doc1 = new PDFDocument("xd.pdf");
             copier.Print(in doc1);
-            IDocument doc2; 
-            copier.Scan(out doc2);
+            IDocument doc3; 
+            copier.Scan(out doc3);
             
             copier.ScanAndPrint();
             Console.WriteLine(copier.PrintCounter);
